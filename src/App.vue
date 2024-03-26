ad873570a52f0e27a37a0c41df3caaa3dc385707
Mario Contreras <mariocc29@gmail.com>
2024-03-26 17:31:19 -0400

    added button component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <section>
+    <article>
+      <Button :btnClass="'btn-info'" :btnLabel="'Info'"/>
+      <Button :btnClass="'btn-success'" :btnLabel="'Success'"/>
+      <Button :btnClass="'btn-warning'" :btnLabel="'Warning'"/>
+      <Button :btnClass="'btn-alert'" :btnLabel="'Alert'"/>
+    </article>
+  </section>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Button from './components/button.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Button
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import '@/styles/fonts';
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100svh;
+    
+    article {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+      gap: 10px;
+    }
+  }
 }
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,59 @@
+<template>
+  <button class="btn" :class="btnClass">{{ btnLabel }}</button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    'btnClass': {
+      type: String
+    },
+    'btnLabel': {
+      type: String
+    }
+  }
+})
+export default class Button extends Vue {
+  btnClass!: string;
+  btnLabel!: string;
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/variables";
+
+.btn {
+  border-radius: 2px;
+	display:inline-block;
+	cursor:pointer;
+	font-size:15px;
+	padding:6px 24px;
+	text-decoration:none;
+
+  &.btn-info {
+    background-color: $btn-primary-bg;
+    border:1px solid $btn-primary-border;
+    color: $btn-primary-text;
+  }
+
+  &.btn-success {
+    background-color: $btn-success-bg;
+    border:1px solid $btn-success-border;
+    color: $btn-success-text;
+  }
+
+  &.btn-warning {
+    background-color: $btn-warning-bg;
+    border:1px solid $btn-warning-border;
+    color: $btn-warning-text;
+  }
+
+  &.btn-alert {
+    background-color: $btn-alert-bg;
+    border:1px solid $btn-alert-border;
+    color: $btn-alert-text;
+  }
+}
+</style>
